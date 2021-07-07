51bad424f3fedbfd1909fc4df8ad835e4cdc8f50
eslam0eid <eeid14108@gmail.com>
2021-07-07 17:23:00 +0200

    upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+
+function openNav() {
+  $('#mySidenav').css('left' , '0')
+  $('.burgerBtn').css('opacity','0')
+}
+
+function closeNav() {
+    $('#mySidenav').css('left' , '-100%')
+    $('.burgerBtn').css('opacity','1')
+}
