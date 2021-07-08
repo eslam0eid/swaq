2d9f3a0ca8d3cc30b4b6aac536ad02c39998781d
eslam0eid <eeid14108@gmail.com>
2021-07-08 19:45:51 +0200

    upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 
+document.querySelector(".toggle-settings img").onclick = function () {
+
+
+  // Toggle Class Open On Main Settings Box
+  document.querySelector(".settings-box").classList.toggle("open");
+  
+};
+
+
 function openNav() {
   $('#mySidenav').css('left' , '0')
   $('.burgerBtn').css('opacity','0')
